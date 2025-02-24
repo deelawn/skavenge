98c95a0cc61fa597a26c3a8317fd31418ac5d23f
deelawn <dboltz03@gmail.com>
2025-02-24 13:11:03 -0800

    hardhat config

---
+++ hardhat.config.js
@@ -0,0 +1,26 @@
+// hardhat.config.js
+module.exports = {
+	solidity: "0.8.19",
+	networks: {
+		hardhat: {
+			accounts: [
+				{
+					privateKey: "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80",
+					balance: "10000000000000000000000" // 10,000 ETH
+				},
+				{
+					privateKey: "0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d",
+					balance: "10000000000000000000000" // 10,000 ETH
+				},
+				{
+					privateKey: "0x5de4111afa1a4b94908f83103eb1f1706367c2e68ca870fc3fb9a804cdab365a",
+					balance: "10000000000000000000000" // 10,000 ETH
+				},
+				{
+					privateKey: "0x7c852118294e51e653712a81e05800f419141751be58f605c371e15141b007a6",
+					balance: "10000000000000000000000" // 10,000 ETH
+				}
+			]
+		}
+	}
+};
